2851fd82b41d4f8f9b58df7b3b6e0b846cf1b1b9
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2022-08-12 21:34:41 +0200

    Initial commit

---
+++ src/main/java/de/hglabor/attackonvillager/AttackOnVillager.java
@@ -0,0 +1,15 @@
+package de.hglabor.attackonvillager;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AttackOnVillager implements ModInitializer {
+    public static final String MOD_ID = "attackonvillager";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
