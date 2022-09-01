14b9a5070d9d9564c9db60f02df99d165edfcc16
mooz <63669478+mooziii@users.noreply.github.com>
2022-09-01 18:04:30 +0200

    Add the :v2 to yarn dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,13 @@ version = "1.0.1"
 
 dependencies {
     minecraft("com.mojang:minecraft:1.19.2")
-    mappings("net.fabricmc:yarn:1.19.2+build.8")
+    mappings("net.fabricmc:yarn:1.19.2+build.8:v2")
     modImplementation("net.fabricmc:fabric-loader:0.14.9")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.60.0+1.19.2")
 }
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 tasks {
     processResources {
